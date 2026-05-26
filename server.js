006abdac582c2b44effcddcea0ff95e43f810806
Aniket Pokale <your-email@gmail.com>
2026-05-26 21:12:50 +0530

    add vercel link

--- server.js
+++ server.js
@@ -2,23 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
 const cors = require('cors');
 const jsonWebToken = require('jsonwebtoken');
+
 const app = express();
+
 const routes = require('./src/route/routes');
 const connectDb = require('./src/dbconnection/db');
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://music-app-topaz-eta.vercel.app"
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/', routes);
 
 connectDb();
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-    
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
